904ab36ffae3829ba3113544afbd8b107d4a3450
Emilien Lemaire <emilien.lemaire@ocamlpro.com>
2024-05-16 15:49:19 +0200

    Fix gen printer

--- src/ocaml/preprocess/printer/gen_printer.ml
+++ src/ocaml/preprocess/printer/gen_printer.ml
@@ -8,13 +8,16 @@ let is_attribute names attr =
 let printf = Printf.printf
 let sprintf = Printf.sprintf
 
-let menhir = "MenhirInterpreter"
+let menhir =
+  let name = Filename.chop_extension (Filename.basename Sys.argv.(1)) in
+  (String.capitalize_ascii name)^".MenhirInterpreter"
+
 
 (** Print header, if any *)
 
 let print_header () =
-  let name = Filename.chop_extension (Filename.basename Sys.argv.(1)) in
-  printf "open %s\n" (String.capitalize_ascii name);
+  (* let name = Filename.chop_extension (Filename.basename Sys.argv.(1)) in *)
+  (* printf "open %s\n" (String.capitalize_ascii name); *)
   List.iter
     (fun attr ->
        if is_attribute ["header"; "printer.header"] attr then
